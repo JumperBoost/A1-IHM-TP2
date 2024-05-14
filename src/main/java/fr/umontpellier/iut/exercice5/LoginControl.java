82cfa4b60f4d4e5d9009a1fabd9b4c8341ae8756
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-05-14 12:12:53 +0200

    Implémentation de l'exercice 5

--- src/main/java/fr/umontpellier/iut/exercice5/LoginControl.java
+++ src/main/java/fr/umontpellier/iut/exercice5/LoginControl.java
@@ -1,5 +1,8 @@
 package fr.umontpellier.iut.exercice5;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -12,22 +15,55 @@ public class LoginControl extends GridPane {
     @FXML
     private PasswordField pwd;
 
+    private StringProperty userIdProperty;
+    private StringProperty pwdProperty;
+
+    @FXML
+    private void initialize() {
+        userIdProperty = new SimpleStringProperty("");
+        pwdProperty = new SimpleStringProperty("");
+        createBindings();
+    }
+
     private void createBindings() {
-        // MÉTHODE À COMPLÉTER
+        userId.textProperty().bindBidirectional(userIdProperty);
+        pwd.textProperty().bindBidirectional(pwdProperty);
+        pwd.editableProperty().bind(Bindings.greaterThanOrEqual(userIdProperty.length(), 6));
+    }
+
+    private boolean checkIfPwdIsValid() {
+        if(pwdProperty.get().length() < 8)
+            return false;
+
+        boolean hasUpperCase = false;
+        boolean hasDigit = false;
+        for(char c : pwdProperty.get().toCharArray()) {
+            if(Character.isUpperCase(c))
+                hasUpperCase = true;
+            else if(Character.isDigit(c))
+                hasDigit = true;
+        }
+        return hasUpperCase && hasDigit;
     }
 
     @FXML
     private void okClicked() {
-        System.out.print(userId.getText() + " ");
-        for (char c : pwd.getText().toCharArray()) {
+        if(!checkIfPwdIsValid())
+            return;
+
+        System.out.print(userIdProperty.get() + " ");
+        for (char c : pwdProperty.get().toCharArray())
             System.out.print("*");
-        }
+
         System.out.println();
     }
 
     @FXML
     private void cancelClicked() {
-        userId.setText("");
-        pwd.setText("");
+        if(userIdProperty.get().isEmpty() && pwdProperty.get().isEmpty())
+            return;
+
+        userIdProperty.set("");
+        pwdProperty.set("");
     }
 }
