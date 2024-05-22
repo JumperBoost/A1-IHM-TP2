4e370e49a1c1604e0b40be5a14e3133c9224238d
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-05-22 16:29:18 +0200

    Fin d'implémentation de l'exercice 6

--- src/main/java/fr/umontpellier/iut/exercice6/ConvertisseurTemperatures.java
+++ src/main/java/fr/umontpellier/iut/exercice6/ConvertisseurTemperatures.java
@@ -1,6 +1,9 @@
 package fr.umontpellier.iut.exercice6;
 
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
@@ -13,16 +16,32 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
 public class ConvertisseurTemperatures extends Application {
 
+    private DoubleProperty celsiusProperty;
+    private DoubleProperty fahrenheitProperty;
+
     @Override
     public void start(Stage primaryStage) {
+        celsiusProperty = new SimpleDoubleProperty(0);
+        fahrenheitProperty = new SimpleDoubleProperty(32);
+
         Label labelCelsius = new Label("°C");
         labelCelsius.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
         Slider sliderCelsius = new Slider(0, 100, 0);
         setSliderAttributes(sliderCelsius);
-        TextField fieldCelsius = new TextField("0,00");
+        sliderCelsius.valueProperty().bindBidirectional(celsiusProperty);
+        celsiusProperty.addListener((observableValue, oldValue, newValue) -> {
+            if((double) newValue > 100)
+                celsiusProperty.setValue(100);
+            else if((double) newValue < 0)
+                celsiusProperty.setValue(0);
+            else fahrenheitProperty.setValue(((double) newValue * 9/5) + 32);
+        });
+        TextField fieldCelsius = new TextField();
+        fieldCelsius.textProperty().bindBidirectional(celsiusProperty, new NumberStringConverter("###.##"));
         fieldCelsius.setPrefWidth(50);
         VBox panneauCelsius = new VBox(30, labelCelsius, sliderCelsius, fieldCelsius);
         panneauCelsius.setAlignment(Pos.CENTER);
@@ -31,7 +50,17 @@ public class ConvertisseurTemperatures extends Application {
         labelFahrenheit.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
         Slider sliderFahrenheit = new Slider(0, 212, 32);
         setSliderAttributes(sliderFahrenheit);
-        TextField fieldFahrenheit = new TextField("32,00");
+        sliderFahrenheit.valueProperty().bindBidirectional(fahrenheitProperty);
+        ChangeListener<Number> fahrenheitListener = (observableValue, oldValue, newValue) -> {
+            if((double) newValue < 32)
+                fahrenheitProperty.setValue(32);
+            else if((double) newValue > 212)
+                fahrenheitProperty.setValue(212);
+            else celsiusProperty.setValue(((double) newValue - 32) * 5/9);
+        };
+        sliderFahrenheit.valueProperty().addListener(fahrenheitListener);
+        TextField fieldFahrenheit = new TextField();
+        fieldFahrenheit.textProperty().bindBidirectional(fahrenheitProperty, new NumberStringConverter("###.##"));
         fieldFahrenheit.setPrefWidth(50);
         VBox panneauFahrenheit = new VBox(30, labelFahrenheit, sliderFahrenheit, fieldFahrenheit);
         panneauFahrenheit.setAlignment(Pos.CENTER);
